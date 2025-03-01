2596f0d6ebda229a1fdc15b570ed321daab4bc58
lucasbeninca <lucasbeninca@proton.me>
2025-03-01 17:28:07 -0300

    update7

--- cypress/e2e/gui/Login.cy.js
+++ cypress/e2e/gui/Login.cy.js
@@ -1,70 +1,62 @@
 import { selectors } from '../../support/selectors';
 
 describe('Automated Tests - GUI - Register', () => {
-    beforeEach(() => {
-        cy.visit('auth/login?redirect=%2F')
-      //  cy.get('#rc-anchor-alert').invoke('remove');
+  beforeEach(() => {
+    cy.visit('auth/login?redirect=%2F')
+  });
 
-    });
-
-it('login with invalid email', ()=> {
-  cy.get(selectors.userInput)
-    .type('123456@email.com') 
-  cy.get('#password')
-    .type(Cypress.env('CYPRESS_PASSWORD'),{log:false})
-  cy.get(selectors.buttonSubmint)
-    .click()
-  cy.get('.message')
-    .contains('Usuário ou senha inválidos')
-    .should('be.visible')
-})
-
-it('login with invalid password', ()=> {
-  cy.get(selectors.userInput)
-    .type(Cypress.env('CYPRESS_EMAIL'),{log:false})  
-  cy.get(selectors.passwordInput)
-    .type('123456')
-  cy.get(selectors.buttonSubmint)
-    .click()
-  cy.get('.message')
-    .contains('Usuário ou senha inválidos')
-    .should('be.visible')
-})
-
-it('login without password', () => {
-  cy.get(selectors.userInput)
-    .type(Cypress.env('CYPRESS_EMAIL'),{log:false}) 
-  cy.get(selectors.buttonSubmint)
-    .click()
-})
-
-it('login without email', () => {
-  cy.get(selectors.passwordInput)
-    .type(Cypress.env('CYPRESS_PASSWORD'),{log:false})
-  cy.get(selectors.buttonSubmint)
-    .click()
-})
-
-it('validate error messages e-mail and password', () => {
-  cy.get(selectors.userInput)
-    .focus()
-    .blur()
-  cy.get('.messageError')
-    .contains('O campo “E-mail“ é obrigatório')
-  cy.get(selectors.passwordInput)
-    .focus()
-    .blur()
-  cy.get('.messageError')
-    .contains(' O campo “Senha“ é obrigatório')    
-})
-
-it('login with valid username and password', () => {
+  it.skip('login with invalid email', ()=> {
     cy.get(selectors.userInput)
-      .type(Cypress.env('CYPRESS_EMAIL'),{log:false}) 
+      .type('123456@email.com') 
     cy.get(selectors.passwordInput)
       .type(Cypress.env('CYPRESS_PASSWORD'),{log:false})
     cy.get(selectors.buttonSubmint)
       .click()
+    cy.get(selectors.messageErrorAlertLogin)
+      .contains('Usuário ou senha inválidos')
+      .should('be.visible')
   })
 
+  it.skip('login with invalid password', ()=> {
+    cy.get(selectors.userInput)
+      .type(Cypress.env('CYPRESS_EMAIL'),{log:false})  
+    cy.get(selectors.passwordInput)
+      .type('123456')
+    cy.get(selectors.buttonSubmint)
+      .click()
+    cy.get(selectors.messageErrorAlertLogin)
+      .contains('Usuário ou senha inválidos')
+      .should('be.visible')
+  })
+
+  it.skip('validate error messages e-mail withoutd', () => {
+    cy.get(selectors.userInput)
+      .focus()
+      .blur()
+    cy.get(selectors.passwordInput)
+      .type(Cypress.env('CYPRESS_PASSWORD'),{log:false})
+    cy.get(selectors.messageErrorInvalidFieldLogin)
+      .contains('O campo “E-mail“ é obrigatório')
+  })
+
+  it.skip('validate error messages password withoutd',() =>{
+    cy.get(selectors.userInput)
+      .type(Cypress.env('CYPRESS_EMAIL'),{log:false}) 
+    cy.get(selectors.passwordInput)
+      .focus()
+      .blur()
+    cy.get(selectors.messageErrorInvalidFieldLogin)
+      .contains('O campo “Senha“ é obrigatório')  
+  })
+
+  it('login with valid username and password', () => {
+      cy.get(selectors.userInput)
+        .type(Cypress.env('CYPRESS_EMAIL'),{log:false}) 
+      cy.get(selectors.passwordInput)
+        .type(Cypress.env('CYPRESS_PASSWORD'),{log:false})
+      cy.get(selectors.buttonSubmint)
+        .click()
+      cy.get('.user__content', { timeout: 5000 })
+      .should('contain', `Olá,`);     
+    })
 })
--- cypress/support/selectors.js
+++ cypress/support/selectors.js
@@ -1,5 +1,7 @@
 export const selectors = {
     userInput: '#user',
     passwordInput: '#password',
-    buttonSubmint: 'button[type="submit"][data-testid="submitButton"]'
+    buttonSubmint: 'button[type="submit"][data-testid="submitButton"]',
+    messageErrorAlertLogin: '.message',
+    messageErrorInvalidFieldLogin: '.errorMessage'
 }
