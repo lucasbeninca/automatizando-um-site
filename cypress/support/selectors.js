6864a037fbb92a9cb5602c658d0729d1003fc9e8
lucasbeninca <lucasbeninca@proton.me>
2025-03-02 14:46:21 -0300

    update10

--- cypress/support/selectors.js
+++ cypress/support/selectors.js
@@ -3,5 +3,26 @@ export const selectors = {
     passwordInput: '#password',
     buttonSubmint: 'button[type="submit"][data-testid="submitButton"]',
     messageErrorAlertLogin: '.message',
-    messageErrorInvalidFieldLogin: '.errorMessage'
+    messageErrorInvalidFieldLogin: '.errorMessage',
+    messageErrorClass: '.errorMessage',
+    name: "#name",
+    lastName: '#lastName',
+    dayOfBirth: '#dayOfBirth',
+    monthOfBirth: '#monthOfBirth',
+    yearOfBirth: '#yearOfBirth',
+    cpf: '#cpf',
+    homePhone: '#homePhone',
+    zipCode: '#zipCode',
+    streetType: '#streetType',
+    streetName: '#streetName',
+    streetNumber: '#streetNumber',
+    neighborhood: '#neighborhood',
+    state: '#state',
+    city: '#city',
+    email: '#email',
+    emailNewsletter: '#emailNewsletter',
+    privacy: '#privacy',
+    buttonRegister: '[qa-auto="gift-wrapping-select-button"]',
+    validateLoginSussefull: '.user__content'
+   
 }
